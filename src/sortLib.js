1067a1b07ea18c2af6064ca98d874f123b308e6e
nagendra <=>
2019-12-28 18:01:01 +0530

    removed else in sortLib

--- src/sortLib.js
+++ src/sortLib.js
@@ -4,11 +4,13 @@ const sortLines = function (content) {
 };
 
 const read = function (err, content) {
-  let sortedContent = '';
-  let error = '';
-  if (err) { error = 'sort : No such file or directory'; }
-  else { sortedContent = sortLines(content); }
-  this.onComplete({ error, sortedContent });
+  if (err) {
+    const error = 'sort : No such file or directory';
+    this.onComplete({ error, sortedContent: '' });
+    return;
+  }
+  const sortedContent = sortLines(content);
+  this.onComplete({ error: '', sortedContent });
 };
 
 const performSort = function (args, readFile, onComplete) {
